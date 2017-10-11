6fb76eae4a1fb7cc26a7ef93e691a4904d43964e
yukun <15152347277@163.com>
2017-10-11 20:45:20 +0800

    登出和登录

--- routes/signout.js
+++ routes/signout.js
@@ -5,7 +5,10 @@ const checkLogin = require('../middlewares/check').checkLogin;
 
 // GET /signout 登出
 router.get('/', checkLogin, function(req, res, next) {
-  res.send(req.flash());
+  // 清空 session 中用户信息
+  req.session.user = null;
+  req.flash('success', '登出成功');
+  res.redirect('/posts');
 });
 
 module.exports = router;
