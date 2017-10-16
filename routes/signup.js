07077460f6970860750c1d9f6e97223825efcf4b
yukun <15152347277@163.com>
2017-10-16 22:50:16 +0800

    测试

--- routes/signup.js
+++ routes/signup.js
@@ -24,19 +24,19 @@ router.post('/', checkNotLogin, function(req, res, next) {
   // 校验参数
   try {
     if (!(name.length >= 1 && name.length <= 10)) {
-      throw new Error('名字请限制在 1-10 个字符');
+      throw new Error('名字请限制在1-10个字符');
     }
     if (['m', 'f', 'x'].indexOf(gender) === -1) {
-      throw new Error('性别只能是 m, f或x');
+      throw new Error('性别只能是m,f或x');
     }
     if (!(bio.length >= 1 && bio.length <= 30)) {
-      throw new Error('个人简介请限制在 1-30 个字符');
+      throw new Error('个人简介请限制在1-30个字符');
     }
     if (!req.files.avatar.name) {
       throw new Error('缺少头像');
     }
     if (password.length < 6) {
-      throw new Error('密码至少 6 个字符');
+      throw new Error('密码至少6个字符');
     }
     if (password !== repassword) {
       throw new Error('两次输入密码不一致');
